1d4efa64ba04dbd8e23b2ff8d2510b9206d205b0
Matt Hamilton <m@tthamilton.com>
2015-12-20 20:04:25 -0500

    add options, help, and variable iterations

--- run.zsh
+++ run.zsh
@@ -1,12 +1,38 @@
 #!/usr/bin/env zsh
 
+typeset -A results
+local spin=('/' '-' '\' '|')
+local test_dir='/tmp/zsh-benchmark'
+local results_dir='/tmp/zsh-benchmark-results'
+local iterations=100
+local usage="${0} [-h] [-n] -- benchmark various zsh frameworks for startup speed.
+
+options:
+    -h  show this help
+    -n  set the number of iterations to run for each framework (default 100)"
+
+while getopts ':hn:' option; do
+  case ${option} in
+    h) print ${usage}
+       return 0
+       ;;
+    n) iterations=${OPTARG}
+       ;;
+    :) print "missing argument for -%s\n" "${OPTARG}" >&2
+       print "${usage}" >&2
+       return 1
+       ;;
+   \?) print "illegal option: -%s\n" "${OPTARG}" >&2
+       print "${usage}" >&2
+       return 1
+       ;;
+   esac
+done
+shift $(( OPTIND - 1 ))
+
 # we will use zpty to run all of the tests asynchronously
 zmodload zsh/zpty
 
-local spin=('/' '-' '\' '|')
-typeset -A results
-local test_dir='/tmp/zsh-benchmark'
-local results_dir='/tmp/zsh-benchmark-results'
 
 # the test_dir will be created by any (and every) framework's init script
 # create the directory for the results.
@@ -42,7 +68,7 @@ benchmark() {
 
   # set up the zpty for the framework
   print -n "\rNow benchmarking ${1}... ${spin[1]}"
-  zpty -b ${1} "ZDOTDIR=${test_dir}/${1} zsh -c \"for i in {1..100}; do {time zsh -ic 'exit' } 2>>! ${results_dir}/${1}.log; done\""
+  zpty -b ${1} "ZDOTDIR=${test_dir}/${1} zsh -c \"for i in {1..${iterations}}; do {time zsh -ic 'exit' } 2>>! ${results_dir}/${1}.log; done\""
   while zpty -t ${1} 2> /dev/null; do
     spin
   done
@@ -56,7 +82,7 @@ benchmark() {
 
 }
 
-print "This may take a LONG time, as it runs each framework startup 100 times"
+print "This may take a LONG time, as it runs each framework startup ${iterations} times"
 print "Average startup times for each framework will be printed as the tests progress.\n"
 sleep 5
 
